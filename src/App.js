3ecdc3610d1daeff7b431c3f80dd7e86e9daa59b
Enio <eniosuarez@gmail.com>
2019-07-12 20:14:14 -0300

    add routes redirect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Detail from './ components/Detail';
 import Welcome from './ components/Welcome';
 import List from './ components/List';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 function App() {
@@ -13,7 +13,8 @@ function App() {
 
       <Switch>
         <Route path='/' exact component={Welcome} />
-        <Route path='/hero/:id' exact component={Detail} />
+        <Route path='/hero/:id' component={Detail} />
+        <Redirect from='*' to='/' />
       </Switch>
     </BrowserRouter>
   );
