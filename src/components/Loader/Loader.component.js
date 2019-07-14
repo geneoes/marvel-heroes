dcb5f7358406d4d70094f69cd457b79d587eef30
Enio <eniosuarez@gmail.com>
2019-07-14 19:34:44 -0300

    add Loader

---
+++ src/components/Loader/Loader.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThirdPartyLoader from 'react-loader-spinner'
+
+function Loader() {
+  return (
+    <div style={{textAlign: 'center', margin: '20px'}}>
+      <ThirdPartyLoader type="Puff" color="#00BFFF" height="100" width="100" />
+    </div>  
+  )  
+}
+
+export default Loader;
